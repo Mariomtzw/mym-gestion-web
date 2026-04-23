4f96969d0a7154ea1e089fd061a582adeaef9d31
flor pensado <florpensado@192.168.1.68>
2026-04-22 22:03:06 -0600

    icono whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       {/* 4. Aplicamos font-sans por defecto a todo el cuerpo de la página */}
       <body className="font-sans antialiased text-white">
         {children}
+        <WhatsAppButton />
       </body>
     </html>
   );
